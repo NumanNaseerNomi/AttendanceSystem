5570f229e77ca0aef53b1d1ec88a5721f99f0c8b
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-04-19 22:45:48 +0500

    change

---
+++ index.php
@@ -0,0 +1,36 @@
+<html>
+  <head>
+    <meta http-equiv="refresh" content="0;url=attendanceBook.php" />
+    <title></title>
+  </head>
+ <body></body>
+</html>
+
+<!-- <script>
+	if (!localStorage.getItem("NOMi.theme"))
+	{
+		localStorage.setItem("NOMi.theme", "dark");
+	}
+
+	function loadTheme(theme)
+	{
+		document.querySelector(":root").setAttribute("color-scheme", theme);
+		document.querySelector("#themeChanger").value = theme;
+	}
+
+	function setTheme(theme)
+	{
+		localStorage.setItem("NOMi.theme", theme);
+		loadTheme(theme);
+	}
+
+	function getCurrentTheme()
+	{
+		return localStorage.getItem("NOMi.theme");
+	}
+
+	window.addEventListener("DOMContentLoaded", () => loadTheme(getCurrentTheme()));
+
+	let themeChanger = document.querySelector("#themeChanger");
+	themeChanger.addEventListener("change", () => setTheme(themeChanger.value));
+</script> -->
