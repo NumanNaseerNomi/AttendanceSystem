40488a1e6a3436378655c07395e8bccdc6e9a68a
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-04-26 02:10:59 +0500

    Update manageDevices.php

--- manageDevices.php
+++ manageDevices.php
@@ -10,18 +10,19 @@
 					<div class="card bg-light text-dark shadow">
 						<div class="card-header text-center"><h5>Add New Device</h5></div>
 						<div class="card-body">
-							<form>
+							<form id="saveDevice">
+								<input type="text" id="id" name="id" hidden>
 								<div class="mb-3">
 									<label for="deviceName" class="form-label">Device Name</label>
-									<input type="text" class="form-control bg-white text-dark" id="deviceName">
+									<input type="text" class="form-control bg-white text-dark" id="deviceName" name="deviceName" required>
 								</div>
 								<div class="mb-3">
 									<label for="deviceID" class="form-label">Device ID</label>
-									<input type="text" class="form-control bg-white text-dark" id="deviceID">
+									<input type="text" class="form-control bg-white text-dark" id="deviceID" name="deviceID" required>
 								</div>
 								<div class="mb-3">
 									<label for="deviceDescription" class="form-label">Device Description</label>
-									<input type="text" class="form-control bg-white text-dark" id="deviceDescription">
+									<input type="text" class="form-control bg-white text-dark" id="deviceDescription" name="deviceDescription">
 								</div>
 								<div class="d-grid gap-2">
 									<button type="submit" class="btn btn-primary btn-sm">Save</button>
@@ -35,7 +36,7 @@
 						<div class="card-header"><h5>Manage Attendance Devices</h5></div>
 						<div class="container-fluid">
 							<div class="card-body table-responsive">
-								<table class="table table-bordered bg-light text-dark align-middle text-nowrap">
+								<table class="table table-bordered table-sm bg-light text-dark align-middle text-nowrap">
 									<thead class="align-middle">
 										<tr>
 											<th scope="col">#</th>
@@ -45,18 +46,7 @@
 											<th scope="col">MANAGE DEVICE</th>
 										</tr>
 									</thead>
-									<tbody>
-										<tr>
-											<td>1</td>
-											<td>Gate-I</td>
-											<td>e38b0be586e50be9</td>
-											<td>Main Gate</td>
-											<td>
-												<button type="button" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></button>
-												<button type="button" class="btn btn-danger btn-sm"><i class="fas fa-trash-alt"></i></button>
-											</td>
-										</tr>
-									</tbody>
+									<tbody id="devicesListTable"></tbody>
 								</table>
 							</dav>
 						</div>
@@ -67,3 +57,131 @@
 		<?php require "components/footer.php"; ?>
 	</body>
 </html>
+
+<script>
+	function plotDevicesListTable(config)
+	{
+  		let row = document.getElementById("devicesListTable").insertRow(-1);
+		row.insertCell(0).innerHTML = config.srNumber;
+		row.insertCell(1).innerHTML = config.userName;
+		row.insertCell(2).innerHTML = config.deviceId;
+		row.insertCell(3).innerHTML = config.description;
+		row.insertCell(4).innerHTML = config.manage;
+	}
+	
+	function fetchDevicesList()
+	{
+		fetch('Database/getDevices.php')
+		.then((response) => response.json())
+		.then((respondedJsonData) =>
+		{
+			for (let i = 0; i < respondedJsonData.length; i++)
+			{
+				let srNum = i + 1;
+				let dataConfig =
+				{
+					srNumber 	: srNum,
+					userName 	: respondedJsonData[i].name,
+					deviceId 	: respondedJsonData[i].deviceId,
+					description : respondedJsonData[i].description,
+					manage 		: '<button type="button" class="btn btn-primary btn-sm" value="' + respondedJsonData[i].id + '" onClick="editDevice(' + respondedJsonData[i].id + ')"><i class="fas fa-edit"></i></button><button type="button" class="btn btn-danger btn-sm" value="' + respondedJsonData[i].id + '" onClick="deleteDevice(' + respondedJsonData[i].id + ')"><i class="fas fa-trash-alt"></i></button>'
+				};
+				
+				plotDevicesListTable(dataConfig);
+			}
+			console.log(respondedJsonData);
+		});
+	}
+	fetchDevicesList();
+</script>
+
+<script>
+	const saveDeviceForm = document.getElementById('saveDevice');
+
+	saveDeviceForm.addEventListener('submit', (event) =>
+	{
+		event.preventDefault();
+
+		const formData =
+		{
+			id					: document.getElementById('id').value,
+			deviceName			: document.getElementById('deviceName').value,
+			deviceID			: document.getElementById('deviceID').value,
+			deviceDescription	: document.getElementById('deviceDescription').value
+		}
+
+		if(formData.id != "")
+		{
+			let alertText = "Are you really want to UPDATE this Device?";
+			
+			if(confirm(alertText) == false)
+			{
+				saveDeviceForm.reset();
+				return;
+			}
+		}
+		
+		fetch('Database/saveDevice.php',
+		{
+			method	: 'POST',
+			headers	: {'Content-Type': 'application/json'},
+			body	: JSON.stringify(formData)
+		})
+		.then((response) => response.json())
+		.then((data) =>
+		{
+			console.log(data);
+			saveDeviceForm.reset();
+			document.getElementById("devicesListTable").innerHTML = "";
+			fetchDevicesList();
+		})
+		.catch((error) => {console.error('Error:', error);});
+	});
+</script>
+
+<script>
+	function editDevice(id)
+	{
+		fetch('Database/getDevice.php',
+		{
+			method	: 'POST',
+			headers	: {'Content-Type': 'application/json'},
+			body	: JSON.stringify(id)
+		})
+		.then((response) => response.json())
+		.then((JsonData) =>
+		{
+			document.getElementById("id").value			= JsonData[0].id;
+			document.getElementById("deviceName").value	= JsonData[0].name;
+			document.getElementById("deviceID").value	= JsonData[0].deviceId;
+			document.getElementById("deviceDescription").value	= JsonData[0].description;
+			console.log(JsonData);
+		})
+		.catch((error) => {console.error('Error:', error);});
+	}
+</script>
+
+<script>
+	function deleteDevice(id)
+	{
+		let alertText = "Are you really want to DELETE this Device?";
+
+		if(confirm(alertText) == true)
+		{
+			fetch('Database/deleteDevice.php',
+			{
+				method	: 'POST',
+				headers	: {'Content-Type': 'application/json'},
+				body	: JSON.stringify(id)
+			})
+			.then((response) => response.json())
+			.then((data) =>
+			{
+				console.log(data);
+				document.getElementById("devicesListTable").innerHTML = "";
+				fetchDevicesList();
+			})
+			.catch((error) => {console.error('Error:', error);});
+		}
+	}
+</script>
